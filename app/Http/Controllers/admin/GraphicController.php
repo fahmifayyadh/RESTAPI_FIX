900c48064a2f66925a09a7319c9303fa74339baf
fahmifayyadh <fahmifayyadh4@gmail.com>
2022-02-24 15:39:05 +0700

    pp

--- app/Http/Controllers/admin/GraphicController.php
+++ app/Http/Controllers/admin/GraphicController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserVisit;
 use Illuminate\Http\Request;
 
 class GraphicController extends Controller
 {
-    //
+    public function index(){
+        $kehadiran = UserVisit::where()
+    }
 }
