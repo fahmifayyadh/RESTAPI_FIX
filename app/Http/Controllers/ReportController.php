dbd57bc7487bc50779632052b26c0d38f6dec8a0
fahmifayyadh <fahmifayyadh4@gmail.com>
2022-06-25 11:18:26 +0700

    test laporan

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,7 +10,7 @@ class ReportController extends Controller
 {
     public function kunjunganadmin(){
         $kunjungan = Place::where('active', 1)
-            ->leftJoin('visitations','place.id', '=', 'places.id' )
+            ->leftJoin('visitations','visitations.place_id', '=', 'places.id' )
             ->select('places.name as destinasi', 'visitations.visitor', 'visitations.date')
             ->orderBy('visitations.date','asc')->get();
     }
